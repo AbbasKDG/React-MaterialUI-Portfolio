bb06d0194c3096b64f7d178d2c60ad5686fc3db7
Abbas <30557675+AbbasKDG@users.noreply.github.com>
2019-07-31 23:07:05 -0600

    sticky top

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import "typeface-roboto";
+import Banner from "./components/Banner/banner.js";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <br />
-      </div>
-    );
-  }
-}
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// import About from "./components/about.js";
+// import Projects from "./components/projects.js";
+
+const App = () => {
+  return (
+    <div>
+      <Banner />
+
+      <br />
+    </div>
+  );
+};
 
 export default App;
+
+// Ask Name of visitor and pass it in as a prop // kind of like a pop up on top of website that blurs website beneath it
+// Route to a AskName Component -Blurred background
+// <Banner />
---
+++ reactportfolio/src/components/Banner/banner.js
@@ -0,0 +1,16 @@
+import "./banner.css";
+import React from "react";
+import "./banner.js";
+
+const Banner = () => {
+  return (
+    <div className="ban">
+      <div className="text">
+        <h1>abbas moiz</h1>
+        <p>Front End Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
